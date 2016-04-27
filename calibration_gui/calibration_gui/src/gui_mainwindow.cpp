b4bc57ef567b59588b2056a83351f661ac779421
davidtvs <davidtvs@ua.pt>
2016-04-27 17:54:53 +0100

    GUI

--- calibration_gui/calibration_gui/src/gui_mainwindow.cpp
+++ calibration_gui/calibration_gui/src/gui_mainwindow.cpp
@@ -8,6 +8,20 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->treeWidget->setColumnCount(1);
+    QList<QTreeWidgetItem *> items;
+    for (int i = 0; i < 10; ++i)
+       items.append(new QTreeWidgetItem((QTreeWidget*)0, QStringList(QString("item: %1").arg(i))));
+    ui->treeWidget->insertTopLevelItems(0, items);
+
+    QWidget *dualPushButtons = new QWidget();
+    QHBoxLayout *hLayout = new QHBoxLayout();
+    hLayout->addWidget(new QPushButton("Button1"));
+    hLayout->addWidget(new QPushButton("Button2"));
+    dualPushButtons->setLayout(hLayout);
+    dualPushButtons->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
+
+    ui->treeWidget->setItemWidget(items.value(1),0,dualPushButtons);
 }
 
 MainWindow::~MainWindow()
@@ -15,11 +29,14 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-// References =========================================================================
+// ====================================================================================
+// References:
 // http://stackoverflow.com/questions/36156519/set-qstring-to-qprocess-properly
 // http://stackoverflow.com/questions/24771293/how-to-get-parameter-from-ros-launch-file-and-use-it-in-qt
 // http://stackoverflow.com/questions/10098980/real-time-display-of-qprocess-output-in-a-textbrowser
 // http://doc.qt.io/qt-4.8/qprocess.html
+// ====================================================================================
+
 void MainWindow::on_pushButton_clicked()
 {
     QProcess process;
